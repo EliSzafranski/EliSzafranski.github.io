504f3d1c30747d6ba0b02c875dc7f362381988a9
EliSzafranski <Eszafranski101@gmail.com>
2021-03-25 23:39:56 -0400

    added wind and humidity

--- projects/JS/weather-app.js
+++ projects/JS/weather-app.js
@@ -15,17 +15,25 @@ $(document).ready(function(){
         var URL = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`
         $.getJSON(URL, function(data){
             updateDOM(data);
-        })
+            console.log(data);
+        });
     }
 
     function updateDOM(data){
         var city = data.name;
-        var temp = Math.round(data.main.temp);
+        var temp = data.main.temp;
+        temp = Math.round((temp * (9/5)) + 32); 
         var desc = data.weather[0].description;
         var icon = data.weather[0].icon;
+        var humd = data.main.humidity;
+        var wind = data.wind.speed;
         $("#city").html(city); 
         $("#temp").html(temp);
-        $("#desc").html(desc);
+        $("#desc").html(`Description: ${desc}`);
         $("#icon").attr("src", icon);
+        $("#humd").html(`Humidity: ${humd}%`);
+        $("#wind").html(`Wind: ${wind}`);
+
+
     }
 });
